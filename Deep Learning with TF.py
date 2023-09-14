bfdfe00c38dce86a9d5f697826b0d32b20bbd124
MM150551 <mina150551@gmail.com>
2023-09-14 03:45:02 +0100

    Project failed, we'll get 'em next time

--- Deep Learning with TF.py
+++ Deep Learning with TF.py
@@ -1,23 +1,40 @@
+
+''''
+after alot of trial and error with this project I've decided to dump it so I can actually get something to work :)
+
+problem: I can't get the input data type right because I'm too dumb and the tutorial I followed was outdated by 6 years
+
+help.
+'''
+
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import pandas as pd
+import numpy as np
 
 # don't get close to this
 print()
-tf.compat.v1.disable_eager_execution()
+print()
+print()
 #######
 
 
 mnist_training , training_info = tfds.load('mnist', split= 'train', shuffle_files=True, download=False, with_info=True)
 mnist_test = tfds.load('mnist', split= 'test', shuffle_files=True, download=False)
 
+
 mnist_training_df = tfds.as_dataframe(mnist_training)
-mnist_test_df = tfds.as_dataframe(mnist_test)
+# mnist_test_df = tfds.as_dataframe(mnist_test)
 
-mnist_training_numpy = tfds.as_numpy(mnist_training)
-mnist_test_numpy = tfds.as_numpy(mnist_test)
+# tf.convert_to_tensor(mnist_training_df)
 
-#print(mnist_test_df.head())
+# print(mnist_training)
+# print()
+
+# mnist_training_numpy = tfds.as_numpy(mnist_training)
+# mnist_test_numpy = tfds.as_numpy(mnist_test)
+
+# print(mnist_test_df.head())
 # print(training_info.)
 
 n_nodes_hl1 = 500
@@ -26,10 +43,27 @@ n_nodes_hl3 = 500
 n_classes = training_info.features["label"].num_classes
 batch_size = 100
 
+def make_normal_ds(PrefetchDataset_object):
+    return tf.data.Dataset.from_tensor_slices(list(PrefetchDataset_object))
+
+
+# mnist_training = make_normal_ds(mnist_training)
+
+# print(mnist_training_numpy)
+print(list(mnist_training))
+
+# def normalize_img(image):
+#     return tf.cast(image, tf.float32) / 255.
+
+
+# #change the image values to float32 data type
+# mnist_training = mnist_training.map(normalize_img, num_parallel_calls = tf.data.AUTOTUNE)
+
+
+tf.compat.v1.disable_eager_execution()
+
 x = tf.compat.v1.placeholder('float')
 y = tf.compat.v1.placeholder('float')
-
-
 
 def neural_network_model(data):
     hidden_1_layer = {'weights':tf.Variable(tf.compat.v1.random_normal([784, n_nodes_hl1])),
@@ -83,4 +117,5 @@ def train_neural_network(x):
 
 
 
-train_neural_network(mnist_training_numpy)
+# train_neural_network(mnist_training)
+
